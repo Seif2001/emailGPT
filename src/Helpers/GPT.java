995e961f3b971652df1044d0a41e123c3e149d0c
Tarek Mohamed Kassab <tatakassab@gmail.com>
2023-12-16 17:34:12 +0200

    Fixed new lines errors with the api

--- src/Helpers/GPT.java
+++ src/Helpers/GPT.java
@@ -1,9 +1,13 @@
 package Helpers;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class GPT {
     private static final String API_KEY = "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";
@@ -12,24 +16,44 @@ public class GPT {
 
     public static String getOpenAIResponse(String prompt) throws Exception {
         HttpClient httpClient = HttpClient.newHttpClient();
+
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("model", "gpt-3.5-turbo");
+
+        JSONArray messagesArray = new JSONArray();
+
+        JSONObject systemMessage = new JSONObject();
+        systemMessage.put("role", "system");
+        systemMessage.put("content", "You are a helpful assistant.");
+
+        JSONObject userMessage = new JSONObject();
+        userMessage.put("role", "user");
+        userMessage.put("content", prompt);
+
+        messagesArray.put(systemMessage);
+        messagesArray.put(userMessage);
+
+        requestBody.put("messages", messagesArray);
+
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(API_ENDPOINT))
                 .header("Content-Type", "application/json")
                 .header("Authorization", "Bearer " + API_KEY)
-                .POST(HttpRequest.BodyPublishers.ofString("{\"model\": \"gpt-3.5-turbo\", \"messages\": [{\"role\": \"system\", \"content\": \"You are a helpful assistant.\"}, {\"role\": \"user\", \"content\": \"" + prompt + "\"}]}"))
+                .POST(HttpRequest.BodyPublishers.ofString(requestBody.toString()))
                 .build();
 
-        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-        if (response.statusCode() == 200) {
-            int start = response.body().indexOf("content") + 11;
-
-            int end = response.body().indexOf("\"", start);
-
-            return response.body().substring(start, end);
-        }
-        else{
-
-            return response.body();
+        try {
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() == 200) {
+                int start = response.body().indexOf("content") + 11;
+                int end = response.body().indexOf("\"", start);
+                return response.body().substring(start, end);
+            } else {
+                return response.body();
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();  // Handle the exception appropriately
+            return "Error occurred during the API request.";
         }
     }
 }
